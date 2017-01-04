764b4395d2635c251abdc5d1731a3c2b4a17ac1a
aitay721924 <aitay721924@gmail.com>
2017-01-04 00:48:05 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace IO
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern bool WritePrivateProfileString(string sectionName, string keyName, string keyValue, string filePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern int GetPrivateProfileString(string sectionName, string keyName, string defaultReturnString, StringBuilder returnString, int returnStringLength, string filePath);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder data = new StringBuilder(255);
+            GetPrivateProfileString("Owner", "name", "NA", data, 255, @".\myConfig.ini");
+            AddListBox(data.ToString());
+        }
+        private void AddListBox(string StrText)
+        {
+            listBox1.Items.Add(StrText);
+        }
+    }
+}
